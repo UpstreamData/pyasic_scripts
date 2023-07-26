b11de3bf2a9f985cb9e29685da8545c76887084c
UpstreamData <75442874+UpstreamData@users.noreply.github.com>
2023-07-25 18:40:43 -0600

    Update install.sh to validate user input, and to place the scripts in /opt/cron_tune.

--- cron_tune/install.sh
+++ cron_tune/install.sh
@@ -1,51 +1,83 @@
 #!/bin/bash
 
-# Initialize an array to store the cron jobs
-cron_jobs=()
-
-# Function to add a new cron job
-function add_cron_job() {
-    echo "Enter the hour of the day to run the script (in 24-hour format, e.g., 0-23):"
-    read hour_of_day
-    echo "Enter the IP address of the miner to set tuning (e.g. 192.168.1.20):"
-    read ip_address
-    echo "Enter the wattage to set on the miner (e.g. 2400):"
-    read wattage
-    cron_time="$hour_of_day * * *"
-    cron_jobs+=("$cron_time /path/to/venv/bin/python /path/to/your_python_file.py $ip_address $wattage")
+# Function to validate the IP address format
+function validate_ip() {
+    local ip=$1
+    local valid_ip_regex="^([0-9]{1,3}\.){3}[0-9]{1,3}$"
+    if [[ ! $ip =~ $valid_ip_regex ]]; then
+        echo "Invalid IP address format. Please try again."
+        return 1
+    fi
 }
 
-# Download the Python file and requirements.txt from GitHub
-wget https://raw.githubusercontent.com/UpstreamData/pyasic_scripts/master/cron_tune/main.py
-wget https://raw.githubusercontent.com/UpstreamData/pyasic_scripts/master/cron_tune/requirements.txt
+# Function to validate the hour of the day
+function validate_hour() {
+    local hour=$1
+    if ! [[ $hour =~ ^[0-9]+$ ]]; then
+        echo "Invalid hour format. Please enter a number between 0 and 23."
+        return 1
+    fi
 
-# Set up a virtual environment
-python3 -m venv venv
-source venv/bin/activate
+    if ((hour < 0 || hour > 23)); then
+        echo "Invalid hour. Please enter a number between 0 and 23."
+        return 1
+    fi
+}
 
-# Install required packages
-pip install -r requirements.txt
+# Function to validate the wattage
+function validate_wattage() {
+    local wattage=$1
+    if ! [[ $wattage =~ ^[0-9]+$ ]]; then
+        echo "Invalid wattage. Please enter a valid number."
+        return 1
+    fi
+}
 
-# Loop to add cron jobs
-while true; do
-    add_cron_job
+# Function to set up everything
+function setup_cron_jobs() {
+    # Download the Python file and requirements.txt from GitHub
+    mkdir -p /opt/cron_tune
+    wget -O /opt/cron_tune/main.py https://raw.githubusercontent.com/UpstreamData/pyasic_scripts/master/cron_tune/main.py
+    wget -O /opt/cron_tune/requirements.txt https://raw.githubusercontent.com/UpstreamData/pyasic_scripts/master/cron_tune/requirements.txt
 
+    # Set up a virtual environment
+    python3 -m venv /opt/cron_tune/venv
+    source /opt/cron_tune/venv/bin/activate
+
+    # Install required packages
+    pip install -r /opt/cron_tune/requirements.txt
+
+    # Loop to add cron jobs
     while true; do
+        echo "Enter the hour of the day to run the script (in 24-hour format, e.g., 0-23):"
+        read hour_of_day
+        validate_hour "$hour_of_day" || continue
+
+        echo "Enter the IP address of the miner to set tuning (e.g. 192.168.1.20):"
+        read ip_address
+        validate_ip "$ip_address" || continue
+
+        echo "Enter the wattage to set on the miner (e.g. 2400):"
+        read wattage
+        validate_wattage "$wattage" || continue
+
+        # Get the path to the virtual environment's Python interpreter
+        venv_python_path=$(which python)
+
+        cron_time="$hour_of_day * * *"
+        cron_job="$cron_time $venv_python_path /opt/cron_tune/main.py $ip_address $wattage"
+
+        (crontab -l ; echo "$cron_job") | crontab -
+
+        echo "Cron job added for $hour_of_day:00 to run your Python script with IP: $ip_address and Wattage: $wattage."
+
         echo "Do you want to add another cron job? (yes/no)"
         read add_another
-        if [[ $add_another == "yes" || $add_another == "no" ]]; then
+        if [[ $add_another != "yes" ]]; then
             break
-        else
-            echo "Invalid input. Please enter 'yes' or 'no'."
         fi
     done
+}
 
-    if [[ $add_another == "no" ]]; then
-        break
-    fi
-done
-
-# Add all the cron jobs to crontab
-for cron_job in "${cron_jobs[@]}"; do
-    (crontab -l ; echo "$cron_job") | crontab -
-done
+# Call the setup function
+setup_cron_jobs
