b0b8950db9ded282576d70f427c85caf18dfb499
UpstreamData <75442874+UpstreamData@users.noreply.github.com>
2023-07-26 17:40:44 -0600

    Update to use logging to log to `/opt/cron_tune/cron_tune.log`

--- cron_tune/README.md
+++ cron_tune/README.md
@@ -8,3 +8,5 @@ Run it automatically on Linux based systems with -
 `curl -sSL https://raw.githubusercontent.com/UpstreamData/pyasic_scripts/master/cron_tune/install.sh -o install.sh && chmod +x install.sh && sudo ./install.sh`
 
 This script installs the cron jobs to `/etc/crontab`, and the python scripts to run to `/opt/cron_tune`.
+
+Logs are located at `/opt/cron_tune/cron_tune.log`, and can be read with `tail /opt/cron_tune/cron_tune.log` 
--- cron_tune/main.py
+++ cron_tune/main.py
@@ -1,14 +1,52 @@
 import pyasic
+
 import sys
 import asyncio
+import logging
+import platform
+
+
+log_file = '/opt/cron_tune/cron_tune.log'
+if platform.system() == 'Windows':
+    # If running on Windows, use a file in the current directory for logging
+    log_file = 'cron_tune.log'
+
+log_level = logging.INFO
+
+logger = logging.getLogger()
+logger.setLevel(log_level)
+file_handler = logging.FileHandler(log_file)
+log_format = '(%(asctime)s)[%(levelname)s] - %(message)s'
+formatter = logging.Formatter(log_format)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 async def main():
     ip = sys.argv[1]
     wattage = sys.argv[2]
 
+    logging.info(f"Setting tuning for miner:\n\t[IP] - {ip}\n\t[Wattage] - {wattage}")
+
     miner = await pyasic.get_miner(ip)
+
+    if miner is None:
+        logging.critical(f"Could not identify miner:\n\t[IP] - {ip}")
+        logging.info(f"Stopping.")
+        exit(-1)
+
+    logging.info(f"Found miner type: {miner}")
+
+    if not miner.supports_autotuning:
+        logging.error(f"Miner type does not support setting power limit: {miner}")
+        logging.info(f"Stopping.")
+        exit(-1)
+
+    logging.info(f"Pushing power limit update: {wattage}W")
+
     await miner.set_power_limit(int(wattage))
+
+    logging.info(f"Done. Miner should now be set to {wattage}W")
 
 
 asyncio.run(main())
